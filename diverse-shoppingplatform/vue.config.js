c39f4d94561aa8e5d45bd8c10287424046d194c2
MForza822 <40931449+MForza822@users.noreply.github.com>
2020-01-19 17:29:50 +0800

    diverse-shoppingplatform

--- diverse-shoppingplatform/vue.config.js
+++ diverse-shoppingplatform/vue.config.js
@@ -2,23 +2,18 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   outputDir: 'dist',
   assetsDir: '',
-  indexPath: '',
   devServer: {
     port: 8080, // 配置端口
     open: true, // 自动开启浏览器
     compress: true, // 开启压缩
-    // 设置让浏览器 overlay 同时显示警告和错误
-    overlay: {
-      warnings: true,
-      errors: true
-    },
+    hotOnly: true // 热更新
     // 设置请求代理
-    proxy: {
-      '/api': {
-        target: '',
-        ws: true,
-        changeOrigin: true
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: '',
+    //     ws: true, // 支持websocket
+    //     changeOrigin: true
+    //   }
+    // }
   }
 }
